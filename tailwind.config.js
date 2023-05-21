f80d3541804bb081c11a4feb8357f8d440f5c09f
Faustino Zanetto <faustizanetto@gmail.com>
2023-05-21 11:18:44 -0300

    feat(core): added base files and packages

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const colors = require('tailwindcss/colors');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: '1rem',
+        screens: {
+          '2xl': '1400px',
+        },
+      },
+      gridTemplateColumns: {
+        aboutMe: 'auto 1fr',
+      },
+      colors: {
+        secondary: colors.cyan,
+        background: colors.zinc,
+        primary:colors.orange,
+      },
+      fontFamily: {
+        sans: ['var(--font-sans)'],
+      },
+    
+    },
+  },
+  // eslint-disable-next-line global-require, import/no-extraneous-dependencies
+  plugins: [, require('tailwindcss-animate')],
+};
